4d5709f17a4fe0c3d7a34b9fc4fd67608b70babc
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-02-17 11:58:31 -0300

    teste 1

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
 import { app } from '../index';
 
 export default function Server() {
-    app.listen(process.env.PORT_SEVER, process.env.HOST, () => {
+    app.listen(process.env.PORT_SEVER, process.env.HOST_LOCAL, () => {
         console.log(`Servidor rodando na rota: http://localhost:${process.env.PORT_SEVER}`);
     });
 }
